fee74c896602260706260d5915185225d37eca3e
goe <goe.alexander@gmail.com>
2021-02-05 15:34:04 +0200

    update config

--- src/main/java/crawlapi/Application.java
+++ src/main/java/crawlapi/Application.java
@@ -9,11 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-/*@EnableJpaRepositories
-@Slf4j
-@EnableAsync
-@EntityScan
-@ComponentScan({"crawlapi"})*/
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
